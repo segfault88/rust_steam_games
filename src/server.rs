7f964b490c6fff8ad7affb8cfd851c33ccff015d
Malcolm Lockyer <segfault88@users.noreply.github.com>
2024-09-25 20:26:48 +1200

    wip

---
+++ src/server.rs
@@ -0,0 +1,25 @@
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+    // // `POST /users` goes to `create_user`
+    // .route("/users", post(create_user));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
